decc88d2e87fd1121b2c0ef817b4a4d17a18a1b2
muiloco <muiloco17@gmail.com>
2019-04-15 09:03:14 -0300

    fix thread

--- servertest.py
+++ servertest.py
@@ -6,7 +6,24 @@ Created on Mon Apr 15 00:51:41 2019
 """
 
 import socket
-import threading               # Import socket module
+import _thread               # Import socket module
+
+class Cliente:
+    cont_cliente=0
+    def __init__(self, cliente, ipcliente, status):
+        self.cliente = cliente
+        self.ipcliente = ipcliente
+        self.status = True
+
+def conexao(con, cliente):
+    print("Conectado:", cliente)
+    while True:     # Now wait for client connection.
+        msg = con.recv(1024)
+        if not msg: break
+        print(msg, cliente)
+    print("Finalizado conexao:", cliente)
+    con.close()
+    _thread.exit()
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
@@ -14,14 +31,10 @@ port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 msg = 'Thank you for connecting'
 
-s.listen(5)
-
-def conexao(x):
-    print(x)
-    while True:     # Now wait for client connection.
-        c, addr = s.accept()     # Establish connection with client.
-        print ('Got connection from', addr)
-        c.send(msg.encode('utf-8'))
-        c.close()                # Close the connection
+s.listen(1)
         
-threading._start_new_thread(conexao,('1'))
+while True:
+    con, cliente = s.accept()
+    _thread.start_new_thread(conexao, tuple([con, cliente]))
+
+s.close()
