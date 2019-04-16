e0f3f985c76471e597362e4deb5c387e8d72d499
muiloco <muiloco17@gmail.com>
2019-04-16 19:57:33 -0300

    Fix

--- servertest.py
+++ servertest.py
@@ -7,9 +7,24 @@ Created on Mon Apr 15 00:51:41 2019
 
 import socket
 import _thread
+import queue
+import hashlib
 
 listaDeCliente = []
+filaDeServico = queue.LifoQueue(maxsize=1000)
 
+#=================classes===============
+class Servico:
+    def __init__(self, inicio, fim, status):
+        self.inicio = inicio
+        self.fim = fim
+        self.status = status
+    
+    def alterarStatus(self, novostatus):
+        self.status = novostatus
+    
+    def retornarRange(self):
+        return self.inicio, self.fim
 
 class Clientes:
     cont_cliente=0
@@ -21,18 +36,15 @@ class Clientes:
     def imprimir(self):
         print("Cliente:", self.ipcliente)
 
-def conexao(con, cliente):
-    print("Conectado:", cliente) 
-    while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print(msg, cliente)
-    print("Finalizado conexao:", cliente)
-    con.close()
-    _thread.exit()
+#=================Metodos de Gerencia do Server=============
 
 def ListaDeClientes(objCliente):
     listaDeCliente.append(objCliente)
+
+def jaECliente(objCliente):
+    if objCliente in listaDeCliente:
+        return False
+    else: return True
 
 def imprimirListaCliente():
     n = len(listaDeCliente)
@@ -42,25 +54,57 @@ def imprimirListaCliente():
         cliente.imprimir()
         x+=1
 
+def gerarFilaDeServicos(qnt_senha):
+    qnt_senhaServico = qnt_senha / 1000
+    cont = 0
+    inicio = 0
+    x = 0
+    while x < qnt_senhaServico:
+        if cont == qnt_senhaServico - 1:
+            fim = cont
+            servico = Servico(inicio, fim, 'Aguardando')
+            filaDeServico.put(servico)
+            inicio = fim + 1
+            cont = 0
+        cont += 1
+        x += 1
 
-s = socket.socket()
+def conexao(con, cliente, senha):
+    print("Conectado:", cliente)
+    servico = filaDeServico.get()
+    inicio, fim = servico.retornarRange()
+    inicio = str(inicio)
+    fim = str(fim)
+    dados = senha + ';' + inicio + ';' + fim
+    con.send(dados.encode('utf-8'))
+    while True:
+        resposta = con.recv(1024)
+        if not msg: break
+        print(msg, cliente)
+    print("Finalizado conexao:", cliente)
+    con.close()
+    _thread.exit()
+
+#===============Servidor Config=====================
+server = socket.socket()
 host = socket.gethostname()
 port = 12345
-s.bind((host, port))
-s.listen(1)
+server.bind((host, port))
+server.listen(1)
 
+#=============Parametros do Servidor===========
 qnt_clientes = input("Insira a quantidade de clintes a serem usado:\n")
 qnt_clientes = int(qnt_clientes)
-n = 0
+senha = input("Senha de 8 caracteres:")
+senha = hashlib.md5(senha.encode()).hexdigest()
+qnt_senha = 10**8
 
+n = 0
+msg = "testeteste"
 while True:
-    if n < qnt_clientes:
-        con, cliente = s.accept()
-        cli = Clientes(con,cliente,True)
+    con, addr = server.accept()
+    cli = Clientes(con,addr,True)
+    if jaECliente(cli):
         ListaDeClientes(cli)
-        n+=1
-        _thread.start_new_thread(conexao, tuple([con, cliente]))
-    elif n==qnt_clientes:
-        imprimirListaCliente()
-        n+=1
-s.close()
+        _thread.start_new_thread(conexao, tuple([con, addr, senha]))
+server.close()
---
+++ teste.py
@@ -0,0 +1,5 @@
+lista = [5,54,6,3]
+x = 3
+if x in lista:
+    print('achou')
+else: print('nao achou')
